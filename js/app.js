9f11f941ac8f65d8f71b1f05684d421b12aa1aba
Mohammed Rakib <mohammedrakibhossain02@gmail.com>
2026-03-08 20:22:37 +0600

    create dashboard navbar and tabs layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log("Dashboard loaded");
+const allTab = document.getElementById("allTab");
+const openTab = document.getElementById("openTab");
+const closedTab = document.getElementById("closedTab");
+
+function setActiveTab(activeTab) {
+  allTab.classList.remove("bg-purple-600", "text-white");
+  openTab.classList.remove("bg-purple-600", "text-white");
+  closedTab.classList.remove("bg-purple-600", "text-white");
+
+  allTab.classList.add("border");
+  openTab.classList.add("border");
+  closedTab.classList.add("border");
+
+  activeTab.classList.add("bg-purple-600", "text-white");
+}
+
+allTab.addEventListener("click", function () {
+  setActiveTab(allTab);
+});
+
+openTab.addEventListener("click", function () {
+  setActiveTab(openTab);
+});
+
+closedTab.addEventListener("click", function () {
+  setActiveTab(closedTab);
+});
