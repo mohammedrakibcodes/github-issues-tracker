538232583daf3db20a5ec9997d606275af9152c3
Mohammed Rakib <mohammedrakibhossain02@gmail.com>
2026-03-09 17:45:01 +0600

    update

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const openTab = document.getElementById("openTab");
 const closedTab = document.getElementById("closedTab");
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
+const noResultsMessage = document.getElementById("noResultsMessage");
 
 let allIssuesData = [];
 
@@ -55,12 +56,20 @@ async function searchIssues() {
 function renderIssueCards(issues) {
   issuesContainer.innerHTML = "";
 
+  if (issues.length === 0) {
+    noResultsMessage.classList.remove("hidden");
+    return;
+  }
+
+  noResultsMessage.classList.add("hidden");
+
   issues.forEach((issue) => {
     const card = createIssueCard(issue);
 
     issuesContainer.appendChild(card);
   });
 }
+
 function createIssueCard(issue) {
   const card = document.createElement("div");
 
@@ -71,7 +80,13 @@ function createIssueCard(issue) {
   const borderColor =
     issue.status === "open" ? "border-green-500" : "border-purple-500";
 
-  card.className = `bg-white rounded-lg shadow p-4 border-t-4 ${borderColor} cursor-pointer`;
+  let priorityColor = "bg-gray-200";
+
+  if (issue.priority == "high") priorityColor = "bg-red-500 text-white";
+  if (issue.priority == "medium") priorityColor = "bg-yellow-400 text-black";
+  if (issue.priority == "low") priorityColor = "bg-green-500 text-white";
+
+  card.className = `bg-white rounded-lg shadow p-4 border-t-4 ${borderColor} cursor-pointer hover:shadow-lg transition`;
 
   card.innerHTML = `
 
@@ -81,7 +96,7 @@ function createIssueCard(issue) {
 ${issue.title}
 </h3>
 
-<span class="text-xs px-2 py-1 bg-gray-100 rounded">
+<span class="text-xs px-2 py-1 rounded ${priorityColor}">
 ${issue.priority}
 </span>
 
@@ -94,13 +109,19 @@ ${issue.description}
 <div class="flex gap-2 mb-3 flex-wrap">
 
 ${issue.labels
-  .map(
-    (label) => `
-<span class="text-xs bg-gray-100 px-2 py-1 rounded">
+  .map((label) => {
+    let labelStyle = "bg-gray-100 text-gray-700";
+
+    if (label == "bug") labelStyle = "bg-red-100 text-red-700";
+    if (label == "help wanted") labelStyle = "bg-yellow-100 text-yellow-700";
+    if (label == "enhancement") labelStyle = "bg-green-100 text-green-700";
+
+    return `
+<span class="text-xs px-2 py-1 rounded ${labelStyle}">
 ${label}
 </span>
-`,
-  )
+`;
+  })
   .join("")}
 
 </div>
@@ -116,6 +137,7 @@ ${label}
 
   return card;
 }
+
 function setActiveTab(tab) {
   allTab.classList.remove("bg-purple-600", "text-white");
   openTab.classList.remove("bg-purple-600", "text-white");
